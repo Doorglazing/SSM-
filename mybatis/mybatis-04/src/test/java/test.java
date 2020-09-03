d226d5291fca52565e92eb0d063e6f76f7384fcd
Doorglazing <35768629+Doorglazing@users.noreply.github.com>
2020-09-03 17:47:13 +0800

    ''

--- mybatis/mybatis-04/src/test/java/test.java
+++ mybatis/mybatis-04/src/test/java/test.java
@@ -1,8 +1,25 @@
+import com.practice.dao.IUserDao;
+import com.practice.domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 public class test {
     @Test
-    void test(){
-        
+    public void test() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = factory.openSession();
+        IUserDao mapper = session.getMapper(IUserDao.class);
+        List<User> users = mapper.findALL();
+        for(User user: users){
+            System.out.println(user);
+        }
     }
 }

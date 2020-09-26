46b234c26faa7962387185ce9a189c835a41448e
Doorglazing <35768629+Doorglazing@users.noreply.github.com>
2020-09-26 20:07:20 +0800

    ''

--- djd/springMytatis02/src/main/java/com/lz/controller/Control.java
+++ djd/springMytatis02/src/main/java/com/lz/controller/Control.java
@@ -5,7 +5,10 @@ import com.lz.domain.Goods01;
 import com.lz.service.BuyGoodsService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -27,9 +30,21 @@ public class Control extends HttpServlet {
         String name = req.getParameter("name");
         String amount = req.getParameter("amount");
         String price = req.getParameter("price");
+//        // 从servletContext中的容器对象中拿创建好的容器对象来用
+//             WebApplicationContext ac = null;
+//        String key = WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE;
+//        Object atr = getServletContext().getAttribute(key);
+//        if(atr != null){
+//            ac = (WebApplicationContext) atr;
+//        }
 
-        String config = "applicationContext.xml";
-        ApplicationContext ac = new ClassPathXmlApplicationContext(config);
+
+        // 用框架中的方法，获取容器对象
+        WebApplicationContext ac = null;
+        ServletContext context = getServletContext();
+        ac = WebApplicationContextUtils.getRequiredWebApplicationContext(context);
+
+
         BuyGoodsService service = (BuyGoodsService) ac.getBean("buyService");
         Goods01 goods01 = new Goods01();
         goods01.setId(Integer.parseInt(id));
